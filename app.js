4f86bb57b04424be89268572531338c4c0ccc28c
Priyanshu Rathore <priyanshurathore0123@gmail.com>
2023-02-04 22:36:48 +0530

    commit

--- app.js
+++ app.js
@@ -24,11 +24,7 @@ mongoose.connection.on("error",(err)=>{
 require("./models/user")
 require("./models/post")
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 app.use(cors())
   
 app.use(express.json())
